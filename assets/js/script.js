91ef010fb439452ae819eacd6db679069f01a613
Ayuri Bessa <ayuribessa@gmail.com>
2022-05-23 18:10:15 -0300

    Feat: removing task with a new remove button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,33 @@
-let id = 0;
+let id = 1;
 document.querySelector('#add-button').addEventListener('click', function addTask() {
    const input = document.getElementById('task-input');
-   const taskDescriptionText = input.value;
+   const taskDescriptionText = document.createElement('p');
    const task = document.createElement('span');
    const lineBreak = document.createElement('br');
    const checkBox = document.createElement('input');
 
+
    //checkBox atributes
    checkBox.type = 'checkbox';
-   checkBox.id = id;
    checkBox.checked = false;
    //task atributes
-   task.textContent = taskDescriptionText;
+   taskDescriptionText.textContent = input.value;
    task.id = id;
+   task.className = 'item';
 
-   document.getElementById('items').append(checkBox, task, lineBreak);
+   task.append(checkBox,taskDescriptionText, lineBreak);
+
+   document.getElementById('items').appendChild(task);
    //clearing the input
    input.value = "";
    id++;
-
 })
 
-// document.querySelector('#rm-button').addEventListener('click' function removeTask(){
-
-// });
+document.querySelector('#rm-button').addEventListener('click', function removeTask(){
+   const checkBoxes = document.querySelectorAll('[type=checkbox]');
+   checkBoxes.forEach(element => {
+      if (element.checked){
+         element.parentElement.remove();
+      }
+   });
+});
